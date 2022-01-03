75f4ffe6df3ba7dd7c91166279de327bbdb975ad
Tim Helmke <timhelmke@aol.com>
2022-01-03 13:32:10 +0100

    docs: add CalendarDay component

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="calendar-day">
+    <!-- Anfang: Template für die Calendar-Event-Component -->
+    <div id="calendar-event">
+      <div class="alert text-center">
+        <div>
+          <strong>Hoch</strong>
+        </div>
+
+        <div>Vue.js 3 lernen</div>
+
+        <div>
+          <i class="fas fa-edit me-2" role="button"></i>
+          <i class="far fa-trash-alt" role="button"></i>
+        </div>
+      </div>
+    </div>
+    <!-- Ende: Template für die Calendar-Event-Component -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalendarDay",
+};
+</script>
+
+<style scoped></style>
--- src/components/CalendarWeek.vue
+++ src/components/CalendarWeek.vue
@@ -6,26 +6,7 @@
           <strong>Montag</strong>
         </div>
         <div class="card-body">
-          <!-- Anfang: Template für die Calendar-Day-Component -->
-          <div id="calendar-day">
-            <!-- Anfang: Template für die Calendar-Event-Component -->
-            <div id="calendar-event">
-              <div class="alert text-center">
-                <div>
-                  <strong>Hoch</strong>
-                </div>
-
-                <div>Vue.js 3 lernen</div>
-
-                <div>
-                  <i class="fas fa-edit me-2" role="button"></i>
-                  <i class="far fa-trash-alt" role="button"></i>
-                </div>
-              </div>
-            </div>
-            <!-- Ende: Template für die Calendar-Event-Component -->
-          </div>
-          <!-- Ende: Template für die Calendar-Day-Component -->
+          <CalendarDay />
         </div>
       </div>
     </div>
@@ -33,8 +14,10 @@
 </template>
 
 <script>
+import CalendarDay from "./CalendarDay.vue";
 export default {
   name: "CalendarWeek",
+  components: { CalendarDay },
 };
 </script>
 
