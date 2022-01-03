189fc7304330a0caa169e567d0828f003c484ab2
Tim Helmke <timhelmke@aol.com>
2022-01-03 13:34:59 +0100

    docs: add CalendarEvent component

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,27 +1,18 @@
 <template>
   <div id="calendar-day">
     <!-- Anfang: Template für die Calendar-Event-Component -->
-    <div id="calendar-event">
-      <div class="alert text-center">
-        <div>
-          <strong>Hoch</strong>
-        </div>
-
-        <div>Vue.js 3 lernen</div>
-
-        <div>
-          <i class="fas fa-edit me-2" role="button"></i>
-          <i class="far fa-trash-alt" role="button"></i>
-        </div>
-      </div>
-    </div>
+    <CalendarEvent />
     <!-- Ende: Template für die Calendar-Event-Component -->
   </div>
 </template>
 
 <script>
+import CalendarEvent from "./CalendarEvent.vue";
 export default {
   name: "CalendarDay",
+  components: {
+    CalendarEvent,
+  },
 };
 </script>
 
