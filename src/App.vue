22b30ce913b3efd58dfcef18314c7e854ffe9aba
Tim Helmke <timhelmke@aol.com>
2022-01-03 13:23:26 +0100

    docs: add CalendarWeek component

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,7 @@
     <div class="row">
       <div class="col-12">
         <!-- Anfang: Template für die Calendar-Week-Component -->
-        <div id="calender-week">
-          <div class="card-group">
-            <div class="card" v-for="i in 7" :key="i">
-              <div class="card-header text-center" role="button">
-                <strong>Montag</strong>
-              </div>
-              <div class="card-body">
-                <!-- Anfang: Template für die Calendar-Day-Component -->
-                <div id="calendar-day">
-                  <!-- Anfang: Template für die Calendar-Event-Component -->
-                  <div id="calendar-event">
-                    <div class="alert text-center">
-                      <div>
-                        <strong>Hoch</strong>
-                      </div>
-
-                      <div>Vue.js 3 lernen</div>
-
-                      <div>
-                        <i class="fas fa-edit me-2" role="button"></i>
-                        <i class="far fa-trash-alt" role="button"></i>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- Ende: Template für die Calendar-Event-Component -->
-                </div>
-                <!-- Ende: Template für die Calendar-Day-Component -->
-              </div>
-            </div>
-          </div>
-        </div>
+        <CalendarWeek />
         <!-- Ende: Template für die Calendar-Week-Component -->
       </div>
     </div>
@@ -119,8 +89,12 @@
 </template>
 
 <script>
+import CalendarWeek from "./components/CalendarWeek.vue";
 export default {
   name: "App",
+  components: {
+    CalendarWeek,
+  },
 };
 </script>
 
