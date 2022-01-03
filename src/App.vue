725e203c725f673d70aa4b5560d21d75531dc4b6
Tim Helmke <timhelmke@aol.com>
2022-01-03 13:41:09 +0100

    docs: add CalendarSettings component

--- src/App.vue
+++ src/App.vue
@@ -16,37 +16,7 @@
             <i class="fas fa-cogs"></i>
           </button>
         </div>
-        <!-- Anfang: Template für die Calendar-Settings-Component -->
-        <div id="calendar-settings">
-          <div class="card">
-            <div class="card-header text-center bg-success text-white">
-              <strong>Einstellungen</strong>
-            </div>
-            <div class="card-body">
-              <ul class="nav nav-pills nav-fill">
-                <li v-for="i in 2" :key="i" class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i class="fas fa-icons text-success"></i
-                  ></a>
-                </li>
-              </ul>
-              <hr />
-              <ul class="nav nav-pills nav-fill">
-                <li class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i class="fas fa-sort-numeric-down-alt text-success"></i
-                  ></a>
-                </li>
-                <li class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i class="fas fa-sort-alpha-down text-success"></i
-                  ></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- Ende: Template für die Calendar-Day-Component -->
+        <CalendarSettings />
       </div>
     </div>
   </div>
@@ -55,11 +25,13 @@
 <script>
 import CalendarWeek from "./components/CalendarWeek.vue";
 import CalendarEntry from "./components/CalendarEntry.vue";
+import CalendarSettings from "./components/CalendarSettings.vue";
 export default {
   name: "App",
   components: {
     CalendarWeek,
     CalendarEntry,
+    CalendarSettings,
   },
 };
 </script>
