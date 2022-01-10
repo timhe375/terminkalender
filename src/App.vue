224cbf8d027b38bd26ae4dd281594a0ef5e21d1a
Tim Helmke <timhelmke@aol.com>
2022-01-10 15:31:49 +0100

    feature: add keep-alive tag

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div class="container-fluid mt-5">
     <div class="row">
       <div class="col-12">
-        <component :is="activeView" />
+        <keep-alive>
+          <component :is="activeView" />
+        </keep-alive>
       </div>
     </div>
     <div class="row mt-3">
